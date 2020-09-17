0d30c637e35c476b47322d0f1edf77b15ee54037
jewer3330 <wujiezhaojun@gmail.com>
2020-09-17 21:14:20 +0800

    修改实现方式

--- client/Assets/ThorEditor/Editor/GameObjectMapEditor.cs
+++ client/Assets/ThorEditor/Editor/GameObjectMapEditor.cs
@@ -15,89 +15,74 @@ using UnityEngine.EventSystems;
 [CanEditMultipleObjects]
 public class GameObjectMapEditor : Editor
 {
-    SerializedProperty mapItemsProp;
-
-    void OnEnable()
-    {
-        mapItemsProp = serializedObject.FindProperty("mapItems");
-    }
+   
 
     public override void OnInspectorGUI()
     {
-        base.OnInspectorGUI();
-        //if (GUILayout.Button("Rename"))
-        //{
-        //    List<Object> objs = new List<Object>();
-
-        //    for (int i = 0;i < mapItemsProp.arraySize; i++)
-        //    {
-        //        var prop = mapItemsProp.GetArrayElementAtIndex(i);
-        //        var objProp = prop.FindPropertyRelative("go");
-        //        if (objProp.objectReferenceValue)
-        //        {
-        //            objs.Add(objProp.objectReferenceValue);
-        //        }
-        //        else
-        //        {
-        //            //mapItemsProp.DeleteArrayElementAtIndex(i);
-        //        }
-        //    }
-
-        //    Dictionary<string, int> counter = new Dictionary<string, int>();
-        //    Dictionary<Object, string> nameRemap = new Dictionary<Object, string>();
-        //    for(int i =0; i < objs.Count;i++)
-        //    {
-        //        int count;
-        //        var r = objs[i];
-        //        if (counter.TryGetValue(r.name, out count))
-        //        {
-        //            counter[r.name] = ++count;
-        //            if (!nameRemap.ContainsKey(r)) //同一个物体被反复添加 
-        //                nameRemap.Add(r, r.name + "" + count);
-        //            else
-        //            {
-        //                mapItemsProp.DeleteArrayElementAtIndex(i);
-        //                counter[r.name] = --count;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            counter.Add(r.name, 0);
-        //            nameRemap.Add(r, r.name);
-        //        }
-        //    }
-
-        //    for (int i = 0; i < mapItemsProp.arraySize; i++)
-        //    {
-        //        var prop = mapItemsProp.GetArrayElementAtIndex(i);
-        //        var nameProp = prop.FindPropertyRelative("name");
-        //        var objProp = prop.FindPropertyRelative("go");
-
-        //        if (objProp.objectReferenceValue)
-        //        {
-        //            var name = objProp.objectReferenceValue.name;
-        //            if (nameRemap.TryGetValue(objProp.objectReferenceValue, out name))
-        //            { 
-        //                nameProp.stringValue = name;
-        //            }
-        //        }
-        //    }
-
-        //}
-
-        if (GUILayout.Button("Export"))
+        GameObjectMap map = target as GameObjectMap;
+        var p = map.GetComponentsInParent<GameObjectMap>(true);
+        if (p.Length > 1)
         {
-            uis.Clear();
-            Export();
+            GUILayout.Label("Sub GameObjectMap");
         }
+        else
+        {
+            if (GUILayout.Button("Export"))
+            {
 
+                Export();
+            }
+        }
+        base.OnInspectorGUI();
         serializedObject.ApplyModifiedProperties();
     }
 
-    public string exportPath => Application.dataPath + "/LuaScripts/";
+    public string exportPath => Application.dataPath + "/LuaScripts/src/UI/Bindings/";
+
+    void Collect(GameObjectMap map, List<UIBehaviour> uis)
+    {
+        var temp = new List<UIBehaviour>();
+        var buttons = map.gameObject.GetComponentsInChildren<Button>(true);
+        var images = map.gameObject.GetComponentsInChildren<Image>(true);
+        var texts = map.gameObject.GetComponentsInChildren<Text>(true);
+        var rawImages = map.gameObject.GetComponentsInChildren<RawImage>(true);
+        var toggles = map.gameObject.GetComponentsInChildren<Toggle>(true);
+        var sliders = map.gameObject.GetComponentsInChildren<Slider>(true);
+        var scrollbars = map.gameObject.GetComponentsInChildren<Scrollbar>(true);
+        var dropdowns = map.gameObject.GetComponentsInChildren<Dropdown>(true);
+        var inputFields = map.gameObject.GetComponentsInChildren<InputField>(true);
+
+        temp.AddRange(buttons);
+        temp.AddRange(images);
+        temp.AddRange(texts);
+        temp.AddRange(rawImages);
+        temp.AddRange(toggles);
+        temp.AddRange(scrollbars);
+        temp.AddRange(dropdowns);
+        temp.AddRange(inputFields);
+        temp.AddRange(sliders);
+
+        temp.ForEach(r =>
+        {
+            var p = r.GetComponentsInParent<GameObjectMap>(true);
+            if (p.Length == 1) //只添加一层导出
+            {
+                uis.Add(r);
+            }
+            else
+            {
+                if (p[0] == map) //如果多层，第一层父节点==导出本身
+                {
+                    uis.Add(r);
+                }
+            }
+        });
+
+    }
 
     public void Export()
     {
+        
         if (!System.IO.Directory.Exists(exportPath))
         {
             System.IO.Directory.CreateDirectory(exportPath);
@@ -105,51 +90,63 @@ public class GameObjectMapEditor : Editor
         if (target)
         {
             GameObjectMap map = target as GameObjectMap;
-            var buttons = map.gameObject.GetComponentsInChildren<Button>(true);
-            var images = map.gameObject.GetComponentsInChildren<Image>(true);
-            var texts = map.gameObject.GetComponentsInChildren<Text>(true);
-            var rawImages = map.gameObject.GetComponentsInChildren<RawImage>(true);
-            var toggles = map.gameObject.GetComponentsInChildren<Toggle>(true);
-            var sliders = map.gameObject.GetComponentsInChildren<Slider>(true);
-            var scrollbars = map.gameObject.GetComponentsInChildren<Scrollbar>(true);
-            var dropdowns = map.gameObject.GetComponentsInChildren<Dropdown>(true);
-            var inputFields = map.gameObject.GetComponentsInChildren<InputField>(true);
-
-           
-
-            Array.ForEach(buttons, r => Add(r));
-            Array.ForEach(images, r => Add(r));
-            Array.ForEach(texts, r => Add(r));
-            Array.ForEach(rawImages, r => Add(r));
-            Array.ForEach(toggles, r => Add(r));
-            Array.ForEach(sliders, r => Add(r));
-            Array.ForEach(scrollbars, r => Add(r));
-            Array.ForEach(dropdowns, r => Add(r));
-            Array.ForEach(inputFields, r => Add(r));
+            var submaps = map.gameObject.GetComponentsInChildren<GameObjectMap>(true);
 
 
-            
+           var uis = new List<UIBehaviour>();
+
+            Collect(map, uis);
+
             using (StreamWriter sw = new StreamWriter(exportPath + target.name + ".lua"))
             {
-                sw.WriteLine("local bindings = function(t)");
-                sw.WriteLine("\t t.bindings = {}");
-                AutoAddMapObject(uis, sw);
+                sw.WriteLine("local ret = function(t)");
+
+               
+                Serialize(uis, sw, map);
+
+                foreach (var k in submaps)
+                {
+                    if (k != map)
+                    {
+                        uis = new List<UIBehaviour>();
+                        Collect(k, uis);
+                        Serialize(uis, sw, k);
+                    }
+                }
+
                 sw.WriteLine("end");
-                sw.WriteLine("return bindings");
+                sw.WriteLine("return ret");
             }
                 
             AssetDatabase.Refresh();
         }
     }
 
-    private string template => "\t t.bindings.{0} = t.map:Get('{0}'):GetComponent(typeof(CS.UnityEngine.UI.{1}))";
-    void Add(UIBehaviour button)
+    private string template => "\t\t table.{0} = map:Get('{0}'):GetComponent(typeof(CS.UnityEngine.UI.{1}))";
+
+
+    string GetFullSubPath(GameObjectMap map)
     {
-        
-        uis.Add(button);
+        var p = map.transform;
+        string ret = p.name;
+
+        GameObjectMap root = target as GameObjectMap;
+
+        while (p.parent)
+        {
+            if (p.GetComponent<GameObjectMap>() == root)
+                break;
+            ret = p.parent.name + "/" + ret;
+            p = p.parent;
+        }
+        if (ret.Contains("/"))
+            ret = ret.Substring(ret.IndexOf("/") + 1);
+        else
+            ret = string.Empty;
+        return ret;
     }
-    private List<UIBehaviour> uis = new List<UIBehaviour>();
-    void AutoAddMapObject(List<UIBehaviour> uis, StreamWriter sw)
+
+    void Serialize(List<UIBehaviour> uis, StreamWriter sw, GameObjectMap map)
     {
         Dictionary<string, int> counter = new Dictionary<string, int>();
         Dictionary<UIBehaviour, string> nameRemap = new Dictionary<UIBehaviour, string>();
@@ -161,9 +158,11 @@ public class GameObjectMapEditor : Editor
             var key = type + "_" + ui.name;
             if (counter.TryGetValue(key, out count))
             {
-                counter[ui.name] = ++count;
-                if (!nameRemap.ContainsKey(ui)) 
+                counter[key] = ++count;
+                if (!nameRemap.ContainsKey(ui))
                     nameRemap.Add(ui, key + "" + count);
+                else
+                    Debug.LogError(ui);
             }
             else
             {
@@ -171,9 +170,11 @@ public class GameObjectMapEditor : Editor
                 nameRemap.Add(ui, key);
             }
         }
-        GameObjectMap map = target as GameObjectMap;
         map.mapItems = new MapItem[nameRemap.Count];
         int c = 0;
+        sw.WriteLine(string.Format("\t t.{0} = {{}}",map.gameObject.name));
+        sw.WriteLine(string.Format("\t t.{0}.map = t.obj.transform:Find('{1}'):GetComponent(typeof(CS.GameObjectMap))", map.gameObject.name, GetFullSubPath(map)));
+        sw.WriteLine(string.Format("\t t.{0}.bind = function(table,map)", map.gameObject.name));
         foreach (var k in nameRemap)
         {
             string name = k.Value.Replace(" ","").Replace("(", "").Replace(")", "");
@@ -187,6 +188,8 @@ public class GameObjectMapEditor : Editor
 
             c++;
         }
+        sw.WriteLine("\t end");
+        sw.WriteLine(string.Format("\t t.{0}.bind(t.{0},t.{0}.map)", map.gameObject.name));
 
         EditorUtility.SetDirty(target);
         AssetDatabase.SaveAssets();
